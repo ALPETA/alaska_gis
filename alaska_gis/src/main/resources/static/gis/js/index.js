b79e209b4276ffe4272714f5805826c8ff3cfa69
JBT <JBT@DESKTOP-2MRUGEK>
2020-11-11 15:31:17 +0900

    각종 validation 처리

--- alaska_gis/src/main/resources/static/gis/js/index.js
+++ alaska_gis/src/main/resources/static/gis/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	//update modal data
 	$("button[name='updatelayer']").click(function() {
 		var layer_data = this.value;
 		layer_data = layer_data.split(',');
@@ -11,4 +12,50 @@ $(document).ready(function() {
 
 	});
 
+	//layer delete
+	$("button[name='deleteLayer']").click(function() {
+		var layernum = this.value;
+		
+		if (confirm("해당 레이어를 삭제하시겠습니까??") == true) {
+			alert("삭제되었습니다.");
+			location.href="/delete/"+layernum;
+		} else {
+			return false;
+		}
+	});
+
 });
+
+//layer insert validation
+function validateinsertForm() {
+	var x = document.forms["insertForm"]["local_name"].value;
+	var y = document.forms["insertForm"]["data_name"].value;
+	if (x == "") {
+		alert("local_name을 입력해 주세요");
+		return false;
+	}
+	else if (y == "") {
+		alert("data_name을 입력해 주세요");
+		return false;
+	} else {
+		alert("레이어가 추가되었습니다.")
+	}
+}
+
+//layer update validation
+function validateupdateForm() {
+	var x = document.forms["updateForm"]["update_local_name"].value;
+	var y = document.forms["updateForm"]["update_data_name"].value;
+	if (x == "") {
+		alert("local_name을 입력해 주세요");
+		return false;
+	}
+	else if (y == "") {
+		alert("data_name을 입력해 주세요");
+		return false;
+	} else {
+		alert("레이어가 수정되었습니다.")
+	}
+}
+
+
