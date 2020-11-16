5da68d0338f65b1590f512751ecf3b6a9fde9aaf
JBT <JBT@DESKTOP-2MRUGEK>
2020-11-16 11:02:56 +0900

    controller 수정

--- src/main/java/com/alaska/test/controller/TestController.java
+++ src/main/java/com/alaska/test/controller/TestController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alaska.test.dto.LineDto;
+import com.alaska.test.dto.PointDto;
+import com.alaska.test.dto.PolygonDto;
+import com.alaska.test.dto.RasterDto;
 import com.alaska.test.dto.TestDto;
 import com.alaska.test.service.TestService;
 
@@ -26,16 +30,22 @@ public class TestController {
 		model.addAttribute("raster", testService.getRaster());
 
 		// polygon
+		model.addAttribute("polygon", testService.getPolygon());
 
 		// point
+		model.addAttribute("point", testService.getPoint());
 
 		// line
+		model.addAttribute("line", testService.getLine());
 
 		model.addAttribute("list", testService.getAll());
 		
 		return "index";
 	}
 
+	
+	
+	
 	// insert
 	@RequestMapping("/insert")
 	private String boardInsertProc(HttpServletRequest request) throws Exception {
@@ -51,13 +61,67 @@ public class TestController {
 	}
 
 	// insert into raster
+	@RequestMapping("/insert/raster")
+	private String boardInsertRaster(HttpServletRequest request) throws Exception {
+
+		RasterDto rasterDto = new RasterDto();
+
+		rasterDto.setLocal_name(request.getParameter("local_name"));
+		rasterDto.setData_name(request.getParameter("data_name"));
+
+		testService.insertRaster(rasterDto);
+
+		return "redirect:/";
+	}
 
 	// insert into polygon
+	@RequestMapping("/insert/polygon")
+	private String boardInsertPolygon(HttpServletRequest request) throws Exception {
+
+		PolygonDto polygonDto = new PolygonDto();
+
+		polygonDto.setLocal_name(request.getParameter("local_name"));
+		polygonDto.setData_name(request.getParameter("data_name"));
+		polygonDto.setFill_color(request.getParameter("fill_color"));
+
+		testService.insertPolygon(polygonDto);
+
+		return "redirect:/";
+	}
 
 	// insert into point
+	@RequestMapping("/insert/polygon")
+	private String boardInsertPoint(HttpServletRequest request) throws Exception {
+
+		PointDto pointDto = new PointDto();
+
+		pointDto.setLocal_name(request.getParameter("local_name"));
+		pointDto.setData_name(request.getParameter("data_name"));
+		pointDto.setFill_color(request.getParameter("fill_color"));
+
+		testService.insertPoint(pointDto);
+
+		return "redirect:/";
+	}
 
 	// insert into line
+	@RequestMapping("/insert/line")
+	private String boardInsertLine(HttpServletRequest request) throws Exception {
 
+		LineDto lineDto = new LineDto();
+
+		lineDto.setLocal_name(request.getParameter("local_name"));
+		lineDto.setData_name(request.getParameter("data_name"));
+		lineDto.setLine_color(request.getParameter("line_color"));
+
+		testService.insertLine(lineDto);
+
+		return "redirect:/";
+	}
+
+	
+	
+	
 	// update
 	@RequestMapping("/updateProc")
 	private String boardUpdateProc(HttpServletRequest request) throws Exception {
@@ -73,19 +137,117 @@ public class TestController {
 	}
 
 	// update raster layer
+	@RequestMapping("/update/raster/name")
+	private String boardUpdateRaster(HttpServletRequest request) throws Exception {
+
+		RasterDto rasterDto = new RasterDto();
+
+		rasterDto.setLocal_name(request.getParameter("local_name"));
+		rasterDto.setData_name(request.getParameter("data_name"));
+
+		testService.updateRaster(rasterDto);
+
+		return "redirect:/";
+	}
 
 	// update polygon layer
+	@RequestMapping("/update/polygon/name")
+	private String boardUpdatePolygon(HttpServletRequest request) throws Exception {
+
+		PolygonDto polygonDto = new PolygonDto();
+
+		polygonDto.setLocal_name(request.getParameter("local_name"));
+		polygonDto.setData_name(request.getParameter("data_name"));
+
+		testService.updatePolygon(polygonDto);
+
+		return "redirect:/";
+	}
 
 	// update point layer
+	@RequestMapping("/update/point/name")
+	private String boardUpdatePoint(HttpServletRequest request) throws Exception {
+
+		PointDto pointDto = new PointDto();
+
+		pointDto.setLocal_name(request.getParameter("local_name"));
+		pointDto.setData_name(request.getParameter("data_name"));
+
+		testService.updatePoint(pointDto);
+
+		return "redirect:/";
+	}
 
 	// update line layer
+	@RequestMapping("/update/line/name")
+	private String boardUpdateLine(HttpServletRequest request) throws Exception {
+
+		LineDto lineDto = new LineDto();
+
+		lineDto.setLocal_name(request.getParameter("local_name"));
+		lineDto.setData_name(request.getParameter("data_name"));
+
+		testService.updateLine(lineDto);
+
+		return "redirect:/";
+	}
+	
+	
+	
 
 	// update polygon style
+	@RequestMapping("/update/polygon/style")
+	private String boardUpdatePolygonStyle(HttpServletRequest request) throws Exception {
+
+		PolygonDto polygonDto = new PolygonDto();
+
+		polygonDto.setLocal_name(request.getParameter("local_name"));
+		polygonDto.setLine_color(request.getParameter("line_color"));
+		polygonDto.setLine_width(Integer.parseInt(request.getParameter("line_width")));
+		polygonDto.setFill_color(request.getParameter("fill_color"));
+		polygonDto.setOpacity(Integer.parseInt(request.getParameter("opacity")));
+
+		testService.updatePolygonStyle(polygonDto);
+
+		return "redirect:/";
+	}
 
 	// update point style
+	@RequestMapping("/update/line/style")
+	private String boardUpdatePointStyle(HttpServletRequest request) throws Exception {
+
+		PointDto pointDto = new PointDto();
+
+		pointDto.setLocal_name(request.getParameter("local_name"));
+		pointDto.setPoint_shape(request.getParameter("point_shape"));
+		pointDto.setFill_color(request.getParameter("fill_color"));
+		pointDto.setPoint_radius(Integer.parseInt(request.getParameter("point_radius")));
+		pointDto.setOpacity(Integer.parseInt(request.getParameter("opacity")));
+		
+		testService.updatePointStyle(pointDto);
+
+		return "redirect:/";
+	}
 
 	// update line style
+	@RequestMapping("/update/line/style")
+	private String boardUpdateLineStyle(HttpServletRequest request) throws Exception {
 
+		LineDto lineDto = new LineDto();
+
+		lineDto.setLocal_name(request.getParameter("local_name"));
+		lineDto.setLine_color(request.getParameter("line_color"));
+		lineDto.setLine_width(Integer.parseInt(request.getParameter("line_width")));
+		lineDto.setOpacity(Integer.parseInt(request.getParameter("opacity")));
+
+		testService.updateLineStyle(lineDto);
+
+		return "redirect:/";
+	}
+
+	
+	
+	
 	// delete
 	@RequestMapping("/delete/{layer_num}")
 	private String boardDelete(@PathVariable int layer_num) throws Exception {
@@ -96,11 +258,39 @@ public class TestController {
 	}
 
 	// delete from raster layer
+	@RequestMapping("/delete/raster/{local_name}")
+	private String boardDeleteRaster(@PathVariable String local_name) throws Exception {
+
+		testService.deleteRaster(local_name);
+
+		return "redirect:/";
+	}
 
 	// delete from polygon layer
+	@RequestMapping("/delete/polygon/{local_name}")
+	private String boardDeletePolygon(@PathVariable String local_name) throws Exception {
+
+		testService.deletePolygon(local_name);
+
+		return "redirect:/";
+	}
 
 	// delete from point layer
+	@RequestMapping("/delete/point/{local_name}")
+	private String boardDeletePoint(@PathVariable String local_name) throws Exception {
+
+		testService.deletePoint(local_name);
+
+		return "redirect:/";
+	}
 
 	// delete from line layer
+	@RequestMapping("/delete/line/{local_name}")
+	private String boardDeleteLine(@PathVariable String local_name) throws Exception {
+
+		testService.deleteLine(local_name);
+
+		return "redirect:/";
+	}
 
 }
